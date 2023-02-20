1fd9ad3b703363156a68093c584ae9c4b58cd0fa
adriannaliu <72051204+adriannaliu@users.noreply.github.com>
2023-02-20 12:42:04 -0500

    completed bar chart

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// setting constants
 const FRAME_HEIGHT = 500;
 const FRAME_WIDTH = 500;
 const MARGINS = { left: 50, right: 50, top: 50, bottom: 50 };
@@ -5,42 +6,62 @@ const MARGINS = { left: 50, right: 50, top: 50, bottom: 50 };
 const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
 const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right;
 
+// frame
 let frame = d3
   .select("#frame")
   .append("svg")
   .attr("height", FRAME_HEIGHT)
   .attr("width", FRAME_WIDTH)
-  .attr("class", "frame");
+  .attr("class", "frame")
+  .append("g")
+  .attr("transform", "translate(" + MARGINS.left + "," + MARGINS.top + ")");
 
+// defining scales for the bar chart
+let xScale = d3.scaleBand().range([0, VIS_WIDTH]).padding(0.4);
+let yScale = d3.scaleLinear().range([VIS_HEIGHT, 0]);
+
+// reading in data
 d3.csv("data/data.csv").then((data) => {
   console.log(data);
 
-  const MAX_VAL = d3.max(data, (d) => {
-    return parseInt(d.Value);
-  });
+  // specifying the domain for the data
+  xScale.domain(
+    data.map(function (d) {
+      return d.Category;
+    })
+  );
+  yScale.domain([
+    0,
+    d3.max(data, function (d) {
+      return d.Value;
+    }),
+  ]);
 
-  const VAL_SCALE = d3.scaleLinear().domain([0, MAX_VAL]).range([0, VIS_WIDTH]);
-
+  // adding the bars
   frame
     .selectAll("bars")
     .data(data)
     .enter()
     .append("rect")
-    .attr("width", 50)
-    .attr("height", (d) => {
-      return VAL_SCALE(d.Value) + MARGINS.top;
+    .attr("class", "bar")
+    .attr("x", function (d) {
+      return xScale(d.Category);
     })
-    .attr("x", 10 + MARGINS.left)
-    .attr("y", VIS_HEIGHT - MARGINS.bottom - MARGINS.top)
-    .attr("fill", "pink")
-    .attr("class", "bar");
+    .attr("y", function (d) {
+      return yScale(d.Value);
+    })
+    .attr("width", xScale.bandwidth())
+    .attr("height", function (d) {
+      return VIS_HEIGHT - yScale(d.Value);
+    })
+    .attr("fill", "pink");
 
+  // add the x axis
   frame
     .append("g")
-    .attr(
-      "transform",
-      "translate(" + MARGINS.left + "," + (VIS_HEIGHT + MARGINS.top + ")")
-    )
-    .call(d3.axisBottom(VAL_SCALE).ticks(4))
-    .attr("font-size", "20px");
+    .attr("transform", "translate(0," + VIS_WIDTH + ")")
+    .call(d3.axisBottom(xScale));
+
+  // add the y axis
+  frame.append("g").call(d3.axisLeft(yScale));
 });
